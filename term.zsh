6655c4605675b24cd981806202aad4a0e58e234e
Leo Liang <leoliang@FHL-MacBook-Pro.twcny.rr.com>
2019-06-25 15:20:53 -0400

    Editor nano

--- term.zsh
+++ term.zsh
@@ -8,6 +8,9 @@ export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 export LS_COLORS="di=36:ln=1;31:so=37:pi=1;33:ex=35:bd=37:cd=37:su=37:sg=37:tw=32:ow=32"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+# Editor
+export EDITOR=nano
+
 # Colourful term output
 alias ls="ls --color=auto -N"
 alias grep="grep --color=auto"
