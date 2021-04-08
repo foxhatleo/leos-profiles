29d015eb1f7b71fa0f0f91e23fdc93dfee0646d4
Wenhao Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-04-08 18:22:18 +0800

    Make yarn bins come before system path

--- path.node.zsh
+++ path.node.zsh
@@ -2,8 +2,8 @@
 
 NPM_PACKAGES="${HOME}/.npm-packages"
 
-export PATH="$PATH:$NPM_PACKAGES/bin"
+export PATH="$NPM_PACKAGES/bin:$PATH"
 
 # Preserve MANPATH if you already defined it somewhere in your config.
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
-export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+export MANPATH="$NPM_PACKAGES/share/man:${MANPATH-$(manpath)}"
