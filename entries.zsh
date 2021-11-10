3b26189adcf681e32712379c10c2409be3909ccd
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-11-10 15:56:53 +0800

    Remove VSC

--- entries.zsh
+++ entries.zsh
@@ -9,7 +9,6 @@ entry "path.thefuck"
 entry "path.ocaml"
 entry "path.node"
 entry "path.gpg"
-entry "path.vsc"
 
 entry "_private" optional
 
--- path.vsc.zsh
+++
@@ -1,2 +0,0 @@
-add-path "$HOME/.vscode-server/bin/f4af3cbf5a99787542e2a30fe1fd37cd644cc31f/bin";
-
