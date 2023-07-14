0013b981bcc934fa6e8a4f35963d1dd166531427
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2023-07-14 15:07:14 -0400

    Fix brew error

--- path.brew.zsh
+++ path.brew.zsh
@@ -1,9 +1,3 @@
-if [ "$(arch)" = "arm64" ]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-    eval "$(/usr/local/bin/brew shellenv)"
-fi
-
 if type "brew" > /dev/null; then
   if [[ -f "$HOME/.brew-china" ]]; then
     export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles";
