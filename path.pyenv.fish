fe56bc10aa39f7bb420a0e928cea0e0c97da09b4
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2024-05-04 13:19:34 -0700

    Add iterm colors

--- path.pyenv.fish
+++ path.pyenv.fish
@@ -10,4 +10,6 @@ if not type -q pyenv
     if not test -f $HOME/.lp-nopyenv
         puts-err "pyenv is not installed. To silence, touch \$HOME/.lp-nopyenv." >&2
     end
+else
+    pyenv init - | source
 end
